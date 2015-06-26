709490870d2074d8cd04398e0a66d80a3ad72e6c
Steven Smith <Steveice10@gmail.com>
2015-06-25 17:55:53 -0700

    Add nor::read.

--- include/citrus/nor.hpp
+++ include/citrus/nor.hpp
@@ -4,6 +4,7 @@
 
 namespace ctr {
     namespace nor {
+        bool read(u32 offset, void* data, u32 size);
         bool write(u32 offset, void* data, u32 size);
     }
 }
--- source/citrus/nor.cpp
+++ source/citrus/nor.cpp
@@ -25,6 +25,15 @@ void ctr::nor::exit() {
     CFGNOR_Shutdown();
 }
 
+bool ctr::nor::read(u32 offset, void* data, u32 size) {
+    if(!initialized) {
+        return false;
+    }
+
+    ctr::err::parse((u32) CFGNOR_ReadData(offset, (u32*) data, size));
+    return !ctr::err::has();
+}
+
 bool ctr::nor::write(u32 offset, void* data, u32 size) {
     if(!initialized) {
         return false;
